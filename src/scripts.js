e687fcc14cd5ebb09f8b4bb880396af00df709de
Alex Peterson <aspeterson.126@gmail.com>
2023-08-01 19:06:54 -0500

    Add fetchTravelerData function with hardcoded currentTraveler variable

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,21 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+var currentTraveler = {
+    id: 1,
+    name: "Ham Leadbeater",
+    travelerType: "relaxer"
+}
+
+const fetchTravelerData = () => {
+    return fetch(`http://localhost:3001/api/v1/travelers`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+console.log(fetchTravelerData())
