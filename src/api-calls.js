64449823b2e3c7f9f0186d70dcdd4bd364919f79
Alex Peterson <aspeterson.126@gmail.com>
2023-08-07 18:03:30 -0500

    Refactor fetch requests to utilize promise.all

---
+++ src/api-calls.js
@@ -0,0 +1,10 @@
+const fetchTravelerData = (dataType) => { 
+    return fetch(`http://localhost:3001/api/v1/${dataType}`)
+     .then(res => res.json())
+ }
+ 
+ export const promises = [
+    fetchTravelerData('travelers'),
+    fetchTravelerData('trips'),
+    fetchTravelerData('destinations')
+ ]
